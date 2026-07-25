872f9baf573a7a4fe63cbc5dd5dd794af5470936
Mohammad El-Tawil <mohammadeltawil0@gmail.com>
2026-07-25 15:56:17 -0400

    Implemented AuthUtil and modified UserRepository

--- ezstub-backend/src/main/java/ezstub_backend/util/AuthUtil.java
+++ ezstub-backend/src/main/java/ezstub_backend/util/AuthUtil.java
@@ -1,4 +1,40 @@
 package ezstub_backend.util;
 
+import ezstub_backend.model.User;
+import ezstub_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AuthUtil {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public String loggedInEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        assert authentication != null;
+        User user = userRepository.findByUserName(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username " + authentication.getName() + "!"));
+        return user.getEmail();
+    }
+
+    public Long loggedInUserId(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        assert authentication != null;
+        User user = userRepository.findByUserName(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username " + authentication.getName() + "!"));
+        return user.getUserId();
+    }
+
+    public User loggedInUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        assert authentication != null;
+        User user = userRepository.findByUserName(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username " + authentication.getName() + "!"));
+        return user;
+    }
 }
