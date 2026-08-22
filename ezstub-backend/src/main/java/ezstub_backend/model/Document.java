0dc9b9a065cc57fc811adf02f620b01b2a02b8b8
Mohammad El-Tawil <mohammadeltawil0@gmail.com>
2026-08-22 15:06:41 -0400

    Modified Document class with more annotations

--- ezstub-backend/src/main/java/ezstub_backend/model/Document.java
+++ ezstub-backend/src/main/java/ezstub_backend/model/Document.java
@@ -1,19 +1,23 @@
 package ezstub_backend.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "documents")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Document extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long documentId;
+
+    @Size(max = 100000)
+    @Column(length = 100000)
     private String note;
 }
