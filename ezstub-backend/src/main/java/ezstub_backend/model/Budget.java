6535bce6738f57b61929af20e7c78d0f7e7a035b
Mohammad El-Tawil <mohammadeltawil0@gmail.com>
2026-09-04 22:24:36 -0400

    Added annotations to Budget model class

--- ezstub-backend/src/main/java/ezstub_backend/model/Budget.java
+++ ezstub-backend/src/main/java/ezstub_backend/model/Budget.java
@@ -12,7 +12,16 @@ import lombok.*;
 import java.math.BigDecimal;
 
 @Entity
-@Table(name = "budgets")
+@Table(name = "budgets",
+        uniqueConstraints = {
+        @UniqueConstraint(
+        name = "uk_budget_user_category_month",
+            columnNames = {
+                    "user_id",
+                    "category",
+                    "month"
+        })
+})
 @Getter
 @Setter
 @NoArgsConstructor
@@ -24,21 +33,24 @@ public class Budget extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long budgetId;
 
-    @NotNull(message = "Category is required")
+    @NotNull(message = "Category is required!")
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
     private ExpenseCategory category; // Food, Travel, etc.
 
-    @NotNull(message = "Limit amount is required")
-    @DecimalMin(value = "0.01", message = "Limit amount must be greater than 0")
+    @NotNull(message = "Limit amount is required!")
+    @DecimalMin(value = "0.01", message = "Limit amount must be greater than 0!")
+    @Column(precision = 12, scale = 2, nullable = false)
     private BigDecimal limitAmount;
 
     @NotBlank(message = "Month is required")
     @Pattern(
             regexp = "^\\d{4}-(0[1-9]|1[0-2])$",
-            message = "Month must be in YYYY-MM format")
+            message = "Month must be in YYYY-MM format!")
+    @Column(nullable = false, length = 7)
     private String month; // "2026-05"
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
